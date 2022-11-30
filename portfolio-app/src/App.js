20a8a018f0764421bd25ff6e8e2d244ce92bbee9
Henry Pham <phamhuyhoang52888@gmail.com>
2022-11-30 12:16:19 -0700

    Finish React App

--- portfolio-app/src/App.js
+++ portfolio-app/src/App.js
@@ -1,9 +1,29 @@
 import './App.css';
+import Header from './Header.js'
+import WorkExpCard from './WorkExperienceCard.js'
+import EduCard from './EducationCard.js'
+import TechSumCard from './TechnologySummaryCard.js'
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <Header />
+    <main className="container">
+        <div className="row mt-3">
+            <div className="col-md">
+            <WorkExpCard />
+            </div>
+            <div className="col-md">
+            <EduCard />
+            </div>
+        </div>
+        <div className="row mt-3">
+            <div className="col-md mb-5">
+            <TechSumCard/>
+            </div>
+        </div>
+      </main>
     </div>
   );
 }
